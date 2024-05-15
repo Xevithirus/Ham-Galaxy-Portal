4c2bbea01ca1e9b5ea8a88771a70d8caf3802747
Xevithirus <xevithirus@gmail.com>
2024-05-15 17:58:00 +1200

    Added countdown timer from w3Schools into play page for no reason

--- script.js
+++ script.js
@@ -1,36 +1,38 @@
 //------------------------------- PARALLAX IMAGE BAR ------------------------------------
 const track = document.getElementById("image-track");
 
-window.onmousedown = e => {
-    track.dataset.mouseDownAt = e.clientX;
-}
+if (track) {  // check if image-track is present to avoid errors on pages without image-track.
+    window.onmousedown = e => {
+        track.dataset.mouseDownAt = e.clientX;
+    }
 
-window.onmouseup = () => {
-    track.dataset.mouseDownAt = "0";
-    track.dataset.prevPercentage = track.dataset.percentage;
-}
+    window.onmouseup = () => {
+        track.dataset.mouseDownAt = "0";
+        track.dataset.prevPercentage = track.dataset.percentage;
+    }
 
-window.onmousemove = e => {
-    if (track.dataset.mouseDownAt === "0") return;
+    window.onmousemove = e => {
+        if (track.dataset.mouseDownAt === "0") return;
 
-    const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
-        maxDelta = window.innerWidth / 2;
-    
-    const percentage = (mouseDelta / maxDelta) * -100,
-        nextPercentage = parseFloat(track.dataset.prevPercentage) + percentage;
-    
-    const limitedNextPercentage = Math.min(0, Math.max(-100, nextPercentage));
+        const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
+            maxDelta = window.innerWidth / 2;
 
-    track.dataset.percentage = limitedNextPercentage;
+        const percentage = (mouseDelta / maxDelta) * -100,
+            nextPercentage = parseFloat(track.dataset.prevPercentage) + percentage;
 
-    track.animate({
-        transform: `translate(${limitedNextPercentage}%, -50%)`
-    }, { duration: 1200, fill: "forwards" });
+        const limitedNextPercentage = Math.min(0, Math.max(-100, nextPercentage));
 
-    for (const image of track.getElementsByClassName("image")) {
-        image.animate({
-            objectPosition: `${100 + limitedNextPercentage}% Center`
+        track.dataset.percentage = limitedNextPercentage;
+
+        track.animate({
+            transform: `translate(${limitedNextPercentage}%, -50%)`
         }, { duration: 1200, fill: "forwards" });
+
+        for (const image of track.getElementsByClassName("image")) {
+            image.animate({
+                objectPosition: `${100 + limitedNextPercentage}% Center`
+            }, { duration: 1200, fill: "forwards" });
+        }
     }
 }
 
@@ -56,3 +58,5 @@ window.onscroll = () => {
         isFixed = false;
     }
 }
+
+
